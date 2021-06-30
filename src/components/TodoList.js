7c06bd9b8093ecfc46468b806e2ade75bdda3e6b
Yilun Xiao <yilunxiao2@gmail.com>
2021-06-30 11:35:34 -0400

    created todolist component and placed on app.js

---
+++ src/components/TodoList.js
@@ -0,0 +1,12 @@
+// Dependencies
+
+// React Component
+const TodoList = () => {
+  return (
+    <div className="Container">
+      This will be the list
+    </div>
+  );
+}
+
+export default TodoList;
